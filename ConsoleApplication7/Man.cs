dfe4535437f0b8da36156e85eec417dfc671b93c
tashaozeruga <tashaozeruga@gmail.com>
2015-03-09 22:04:02 +0200

    Properties lecture

---
+++ ConsoleApplication7/Man.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Man
+    {
+        private string name;
+        public string Name 
+        {
+            set
+            {
+                if (value != "") name = value;
+            }
+            get
+            {
+                return name;
+            }
+            
+           }
+        int age;
+
+        public int Age
+        {
+            set
+            {
+                if (value > 0 && value < 100) 
+                age = value;
+            }
+            get
+            {
+                return age;
+            }
+        }
+        public Man(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+
+        }
+         
+         
+    }
+}
---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Man m1 = new Man("Alex", 30);
+            Man m2 = new Man("Peter", 25);
+
+            m1.Name = "Vasya";
+            m2.Name = "";
+
+
+            m1.Age = 5;
+            m2.Age -= 30;
+
+            Console.WriteLine(m1.Name + ": " + m1.Age);
+            Console.WriteLine(m2.Name + ": " + m2.Age);
+
+
+
+        }
+    }
+}
